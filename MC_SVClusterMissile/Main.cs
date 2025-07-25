f8a7f2c5f55b8d5caa4918b5644739643ba4d0b1
MPC88 <myemail@gmail.com>
2025-07-25 22:24:43 +0100

    Add project files.

---
+++ MC_SVClusterMissile/Main.cs
@@ -0,0 +1,113 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+namespace MC_SVClusterMissile
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class Main : BaseUnityPlugin
+    {
+        // BepInEx
+        public const string pluginGuid = "mc.starvalor.clustermissile";
+        public const string pluginName = "SV Cluster Missile";
+        public const string pluginVersion = "0.0.1";
+
+        private static int num = 0;
+
+        public void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(Main));
+            Harmony.CreateAndPatchAll(typeof(ShotgunMissileControl));
+        }
+
+        [HarmonyPatch(typeof(Weapon), nameof(Weapon.Fire))]
+        [HarmonyPatch(typeof(Weapon), "FireExtra")]
+        [HarmonyPrefix]
+        private static void WeaponFire_Pre(out bool __state, Weapon __instance, bool ___isDrone, SpaceShip ___ss, float ___currCoolDown, float ___chargedFireCount, AmmoBuffer ___ammoBuffer, CargoSystem ___cs)
+        {
+            __state = true;
+
+            // Only leave state true if Weapon.Fire will get as far as ProjectileControl.Fire call
+            if ((!___isDrone && ___ss.energyMmt.valueMod(0) == 0f) || 
+                (__instance.chargeTime > 0f && !(___chargedFireCount > 0f)) || 
+                !(___currCoolDown <= 0f) || 
+                !CanPayCost(__instance, ___isDrone, ___ss, ___ammoBuffer, ___cs))
+                __state = false;
+        }
+
+        [HarmonyPatch(typeof(Weapon), nameof(Weapon.Fire))]
+        [HarmonyPatch(typeof(Weapon), "FireExtra")]
+        [HarmonyPostfix]
+        private static void WeaponFire_Post(Weapon __instance, ProjectileControl ___projControl, bool __state)
+        {
+            if (!__state)
+                return;
+
+            if (__instance.wRef.type == WeaponType.Missile)
+                // TODO: ___projControl == null in FireExtras
+                ___projControl.gameObject.AddComponent(typeof(ShotgunMissileControl));
+        }
+
+        private static bool CanPayCost(Weapon instance, bool isDrone, SpaceShip ss, AmmoBuffer ammoBuffer, CargoSystem cs)
+        {
+            if (isDrone)
+            {
+                return true;
+            }
+            if (instance.wRef.energyCost != 0f && instance.wRef.energyCost * ss.energyMmt.energyMod(0) > ss.stats.currEnergy)
+            {
+                return false;
+            }
+            if (ammoBuffer != null && !CanPayAmmo(ammoBuffer, instance.wRef.ammo.qnt))
+            {
+                if (cs == null || !CanReplicateAmmo(cs, instance.wRef.ammo.itemID, ss))
+                {
+                    return false;
+                }
+                if (!CanPayAmmo(ammoBuffer, instance.wRef.ammo.qnt))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool CanPayAmmo(AmmoBuffer ammoBuffer, int qntToPay)
+        {
+            if (ammoBuffer is AmmoBuffer_CargoSystem)
+            {
+                AmmoBuffer_CargoSystem abcs = ammoBuffer as AmmoBuffer_CargoSystem;
+                if (abcs.qnt >= qntToPay)
+                {
+                    return true;
+                }
+                return false;
+            }
+            else if (ammoBuffer is AmmoBuffer_ItemStock)
+            {
+                AmmoBuffer_ItemStock abis = ammoBuffer as AmmoBuffer_ItemStock;
+                if (abis.isd != null && abis.isd.stock >= qntToPay)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            return false;
+        }
+
+        private static bool CanReplicateAmmo(CargoSystem cs, int ammoID, SpaceShip ss)
+        {
+            if (ss.stats.modelData.manufacturer != TFaction.Miners)
+                return false;
+
+            int num = cs.ExistItemInShipCargo(3, 42, 1);
+            if (num >= 1)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ MC_SVClusterMissile/ShotgunMissileControl.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UIElements;
+using HarmonyLib;
+
+namespace MC_SVClusterMissile
+{
+    class ShotgunMissileControl : MonoBehaviour
+    {
+        private const float DEPLOY_TIME = 3.0f;
+        private const float ARC = 20f;
+        private const int NUMCHILDREN = 5;
+
+        private static int projIndex = -1;
+
+        internal float elapsedTime = 0;
+
+        private ProjectileControl originalProjCont;        
+
+        public void Awake()
+        {
+            // Projectile names
+            // Bullet, Laser_Blue, Cannon_Bullet, Bullet, Laser_Red, Laser_Purple, Laser_Green,
+            // Laser_Cyan, Plasma_Blast, Missle_1, Missle_2, Mine_1, Plasma_Torpedo, Quantum_Pulse,
+            // Railgun_Bullet, Laser_Green, Orb
+            if(projIndex == -1)
+                projIndex = GameManager.instance.GetProjectilePoolIndex("Cannon_Bullet");
+        }
+
+        public void Start()
+        {
+            originalProjCont = this.gameObject.GetComponent<ProjectileControl>();
+            elapsedTime = 0;
+        }
+
+        public void Update()
+        {
+            elapsedTime += Time.deltaTime;
+            if(elapsedTime >= DEPLOY_TIME)
+            {
+                float rotOffset = 0 - (ARC/2);
+                for (int i = 0; i < NUMCHILDREN; i++)
+                {
+                    SpawnChildProjectile(Vector3.zero, rotOffset);
+                    rotOffset += ARC/NUMCHILDREN;
+                }
+                if (originalProjCont.hasEntity)
+                    ((Entity)AccessTools.Field(typeof(ProjectileControl), "entity").GetValue(originalProjCont)).Die();
+                else
+                    originalProjCont.DisableAndHide();
+            }
+        }
+
+        private void SpawnChildProjectile(Vector3 posOffset, float rotOffset)
+        {
+            Vector3 pos = originalProjCont.gameObject.transform.position + posOffset;
+            GameObject gameObject = GameManager.instance.SpawnProjectile(ShotgunMissileControl.projIndex, pos, originalProjCont.gameObject.transform.rotation);
+            gameObject.transform.Rotate(new Vector3(0, 1, 0), rotOffset);
+            ProjectileControl projControl = gameObject.GetComponent<ProjectileControl>();
+            projControl.target = originalProjCont.target;
+            projControl.damage = originalProjCont.damage;
+
+            projControl.SetFFSystem(originalProjCont.ffSys);
+
+            //audioS.PlayOneShot(audioToPlay, SoundSys.SFXvolume * (isDrone ? 0.3f : 1f) * audioMod);
+            projControl.aoe = originalProjCont.aoe;
+            projControl.transform.localScale = originalProjCont.transform.localScale;
+
+            projControl.tDmg = originalProjCont.tDmg;
+            projControl.impact = originalProjCont.impact;
+            projControl.speed = originalProjCont.speed;
+                        
+            float newProxDmgMod = (float)AccessTools.FieldRefAccess<float>(typeof(ProjectileControl), "proximityDmgMod")(projControl);
+            newProxDmgMod = (float)AccessTools.Field(typeof(ProjectileControl), "proximityDmgMod").GetValue(originalProjCont);
+            if (newProxDmgMod != 0f)
+            {
+                Vector3 newSpawnPos = (Vector3)AccessTools.FieldRefAccess<Vector3>(typeof(ProjectileControl), "spawnPosition")(projControl);
+                newSpawnPos = base.transform.position;
+
+                float newMaxRange = (float)AccessTools.FieldRefAccess<float>(typeof(ProjectileControl), "maxRange")(projControl);
+                newMaxRange = (float)AccessTools.Field(typeof(ProjectileControl), "maxRange").GetValue(originalProjCont);
+            }
+
+            projControl.timeToDestroy = originalProjCont.timeToDestroy;
+            projControl.explodeOnDestroy = originalProjCont.explodeOnDestroy;
+            projControl.damageType = originalProjCont.damageType;
+            projControl.ownerSS = originalProjCont.ownerSS;
+            projControl.canHitProjectiles = originalProjCont.canHitProjectiles;
+            projControl.piercing = originalProjCont.piercing;
+            Rigidbody originalRB = (Rigidbody)AccessTools.Field(typeof(ProjectileControl), "rb").GetValue(originalProjCont);
+            projControl.SetRbVelocity(originalRB.velocity);
+
+            //ApplyProjectileMods_Missile(projControl);
+
+            projControl.Fire();
+        }
+
+        private void ApplyProjectileMods_Missile(ProjectileControl projControl)
+        {
+            projControl.turnSpeed = originalProjCont.turnSpeed;
+            projControl.homing = originalProjCont.homing;
+            projControl.autoTargeting = originalProjCont.autoTargeting;
+            projControl.timeToDestroy += 0.48f;
+        }
+
+        [HarmonyPatch(typeof(ProjectileControl), nameof(ProjectileControl.DisableAndHide))]
+        [HarmonyPrefix]
+        private static void ProjContDisableHide_Pre(ProjectileControl __instance)
+        {
+            // Remove this component before projectile is added back into object pool and
+            // before gameobject is disabled.
+            ShotgunMissileControl smc = __instance.gameObject.GetComponent<ShotgunMissileControl>();
+            if (smc != null)
+                Object.Destroy(smc);
+        }
+    }
+}
