77fd7128a659099d73c3c5779732a43b1ac46d93
MPC88 <myemail@gmail.com>
2025-07-26 20:00:23 +0100

    Corrected projcont entity field reference.  Removed misile specific mods method.  Moved get for original projectile controller to time needed.

--- MC_SVClusterMissile/ShotgunMissileControl.cs
+++ MC_SVClusterMissile/ShotgunMissileControl.cs
@@ -1,10 +1,5 @@
 ﻿using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
-using UnityEngine.SocialPlatforms;
-using UnityEngine.UIElements;
 using HarmonyLib;
-using System.Collections;
-using System.Diagnostics;
 using System.Reflection;
 
 namespace MC_SVClusterMissile
@@ -20,32 +15,23 @@ namespace MC_SVClusterMissile
         private const float ARC = 20f;
         private const int NUMCHILDREN = 5;
 
-        private static FieldInfo projContEntityField = AccessTools.Field(typeof(ProjectileControl), "Entity");
+        private static FieldInfo projContEntityField = AccessTools.Field(typeof(ProjectileControl), "entity");
         private static FieldInfo projContProxDmgModField = AccessTools.Field(typeof(ProjectileControl), "proximityDmgMod");
         private static FieldInfo projContSpawnPos = AccessTools.Field(typeof(ProjectileControl), "spawnPosition");
         private static FieldInfo projContMaxRange = AccessTools.Field(typeof(ProjectileControl), "maxRange");
         private static int projIndex = -1;
 
         private float elapsedTime = 0;
-        private ProjectileControl originalProjCont;     
-
-        public void Awake()
-        {            
-            if(projIndex == -1)
-                projIndex = GameManager.instance.GetProjectilePoolIndex(PROJECTILE);
-        }
 
         public void Reset()
         {
-            originalProjCont = this.gameObject.GetComponent<ProjectileControl>();
+            if (projIndex == -1)
+                projIndex = GameManager.instance.GetProjectilePoolIndex(PROJECTILE);
             elapsedTime = 0;
         }
 
         public void Update()
         {
-            if (!enabled)
-                return;
-
             elapsedTime += Time.deltaTime;
             if(elapsedTime >= DEPLOY_TIME)
             {
@@ -53,12 +39,15 @@ namespace MC_SVClusterMissile
                     return;
                 Main.allowClusterThisFrame = false;
 
+                ProjectileControl originalProjCont = this.gameObject.GetComponent<ProjectileControl>();
+                
                 float rotOffset = 0 - (ARC/2);                
                 for (int i = 0; i < NUMCHILDREN; i++)
                 {
-                    SpawnChildProjectile(Vector3.zero, rotOffset);
+                    SpawnChildProjectile(originalProjCont, Vector3.zero, rotOffset);
                     rotOffset += ARC/NUMCHILDREN;
                 }
+
                 if (originalProjCont.hasEntity)
                     ((Entity)projContEntityField.GetValue(originalProjCont)).Die();
                 else
@@ -66,12 +55,13 @@ namespace MC_SVClusterMissile
             }
         }
 
-        private void SpawnChildProjectile(Vector3 posOffset, float rotOffset)
+        private void SpawnChildProjectile(ProjectileControl originalProjCont, Vector3 posOffset, float rotOffset)
         {
             Vector3 pos = originalProjCont.gameObject.transform.position + posOffset;
             GameObject gameObject = GameManager.instance.SpawnProjectile(ShotgunMissileControl.projIndex, pos, originalProjCont.gameObject.transform.rotation);
             gameObject.transform.Rotate(new Vector3(0, 1, 0), rotOffset);
             ProjectileControl projControl = gameObject.GetComponent<ProjectileControl>();
+
             projControl.target = originalProjCont.target;
             projControl.damage = originalProjCont.damage;
 
@@ -80,7 +70,6 @@ namespace MC_SVClusterMissile
             //audioS.PlayOneShot(audioToPlay, SoundSys.SFXvolume * (isDrone ? 0.3f : 1f) * audioMod);
             projControl.aoe = originalProjCont.aoe;
             projControl.transform.localScale = originalProjCont.transform.localScale;
-
             projControl.tDmg = originalProjCont.tDmg;
             projControl.impact = originalProjCont.impact;
             projControl.speed = originalProjCont.speed;
@@ -103,18 +92,11 @@ namespace MC_SVClusterMissile
             projControl.piercing = originalProjCont.piercing;
             Rigidbody originalRB = (Rigidbody)AccessTools.Field(typeof(ProjectileControl), "rb").GetValue(originalProjCont);
             projControl.SetRbVelocity(originalRB.velocity);
-
-            //ApplyProjectileMods_Missile(projControl);
-
-            projControl.Fire();
-        }
-
-        private void ApplyProjectileMods_Missile(ProjectileControl projControl)
-        {
             projControl.turnSpeed = originalProjCont.turnSpeed;
             projControl.homing = originalProjCont.homing;
             projControl.autoTargeting = originalProjCont.autoTargeting;
-            projControl.timeToDestroy += 0.48f;
+
+            projControl.Fire();
         }
 
         [HarmonyPatch(typeof(ProjectileControl), nameof(ProjectileControl.DisableAndHide))]
